94cc303cd95966b87c46ad7dc8e6fc2c88330e02
Kaleb Redman <kaleb@redman.us>
2021-03-28 19:57:09 -0400

    Migrate header to seperate component

---
+++ components/header.js
@@ -0,0 +1,13 @@
+export default function Header({ text }) {
+    if (!text) {
+        return null
+    } else {
+        return (
+            <header className="flex items-center justify-center p-8">
+                <div className="flex items-center">
+                    <span className="text-gray-800 text-3xl font-semibold">{text}</span>
+                </div>
+            </header>
+        )
+    }
+}
--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,21 @@
 import Sidebar from './sidebar'
+import Header from './header'
 
-export default function Layout({ children }) {
+export default function Layout({ header, children }) {
     return (
         <>
             <div className="flex h-screen bg-gray-100 font-roboto">
                 <Sidebar />
-                <div className="flex-1 flex flex-col overflow-hidden">{children}</div>
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    <Header text={header} />
+                    <main className="flex-1 overflow-x-hidden overflow-y-auto">
+                        <div className="container mx-auto px-6 py-8">
+                            <div className="grid place-items-center h-96 text-gray-500 text-xl border-4 border-gray-300 border-dashed">
+                                {children}
+                            </div>
+                        </div>
+                    </main>
+                </div>
             </div>
         </>
     )
