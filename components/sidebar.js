2bb60e5dc10ab6834296db9873c3fe3cbeb99f62
Kaleb Redman <kaleb@redman.us>
2021-03-28 18:50:03 -0400

    Begin writing Sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,3 +1,11 @@
 export default function Sidebar() {
-    return <div>SIDEBAR</div>
+    return (
+        <div className="fixed z-30 inset-y-0 left-0 w-60 transition duration-300 transform bg-white overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
+            <div className="flex items-center justify-center mt-8">
+                <div className="flex items-center">
+                    <span className="text-gray-800 text-2xl font-semibold">Kaleb Redman</span>
+                </div>
+            </div>
+        </div>
+    )
 }
