561e5e1de6cbfd92bc024080cbee355576b855d4
Kaleb Redman <kaleb@redman.us>
2021-03-28 18:49:37 -0400

    Modify Layout for flex

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,10 @@ import Sidebar from './sidebar'
 export default function Layout({ children }) {
     return (
         <>
-            <Sidebar />
-            <div>{children}</div>
+            <div className="flex h-screen bg-gray-100 font-roboto">
+                <Sidebar />
+                <div className="flex-1 flex flex-col overflow-hidden">{children}</div>
+            </div>
         </>
     )
 }
