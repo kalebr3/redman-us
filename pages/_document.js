4306426a2c21c4a86849fcc132fa553a6ccdfc84
Kaleb Redman <8550094-kalebr3@users.noreply.gitlab.com>
2021-04-01 19:30:52 -0400

    Simplify MyDocument and add lang to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="stylesheet" href="https://use.typekit.net/wmo8lvj.css" />
         </Head>
