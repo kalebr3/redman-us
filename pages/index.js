069ee84d1a725efdc80d5be18325b1adab8f4632
Kaleb Redman <8550094-kalebr3@users.noreply.gitlab.com>
2021-04-06 17:59:25 -0400

    add storyblok data fetch to index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import Layout from 'components/layout'
 
-export default function HomePage() {
+import Storyblok from 'lib/storyblok'
+
+export default function Home(props) {
     return (
         <Layout header="About Me">
             <div className="grid place-items-center h-96 text-gray-500 text-xl">
-                HELLO WORLD!
+                { props.story ? props.story.name : 'HELLO WORLD!' }
             </div>
         </Layout>
     )
+}
+
+export async function getStaticProps(context) {
+    let slug = "home"
+
+    let params = {
+        version: "draft",
+    }
+
+    if (context.preview) {
+        params.version = "draft"
+        params.cv = Date.now()
+    }
+
+    let { data } = await Storyblok.get(`cdn/stories/${slug}`, params)
+
+    return {
+        props: {
+            story : data ? data.story : false,
+            preview: context.preview || false
+        },
+        revalidate: 10,
+    }
 }
