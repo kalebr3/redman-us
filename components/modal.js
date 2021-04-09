f1a835d4f406a0bea87c8e3f54936612549b8676
Kaleb Redman <kaleb@redman.us>
2021-04-09 15:56:32 -0400

    update modal

--- components/modal.js
+++ components/modal.js
@@ -3,8 +3,8 @@ import CloseIcon from 'components/svg/closeIcon'
 export default function Modal({ onClick, header, body, footer }) {
     return (
         <div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black" onClick={onClick}></div>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed z-50 outline-none">
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed z-50 outline-none inset-0">
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     {/*content*/}
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none">
