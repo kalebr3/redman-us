f80aae8ff7c7104a8532654298f3c17debac4da4
Kaleb Redman <kaleb@redman.us>
2021-04-09 10:54:18 -0400

    fix modal close action

--- components/modal.js
+++ components/modal.js
@@ -2,9 +2,9 @@ import CloseIcon from 'components/svg/closeIcon'
 
 export default function Modal({ onClick, header, body, footer }) {
     return (
-        <>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none" onClick={onClick}>
+        <div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black" onClick={onClick}></div>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed z-50 outline-none">
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     {/*content*/}
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none">
@@ -54,6 +54,6 @@ export default function Modal({ onClick, header, body, footer }) {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
