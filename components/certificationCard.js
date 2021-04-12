6b528289abf8f01b4b7ae108a125338fb29a6869
Kaleb Redman <kaleb@redman.us>
2021-04-12 18:23:49 -0400

    move date format to reusable function

--- components/certificationCard.js
+++ components/certificationCard.js
@@ -1,10 +1,7 @@
+import formatDate from 'lib/formatDate'
+
 export default function CertificationCard({ blok }) {
-    const strDate = blok.date;
-    const rawDate = new Date(strDate.slice(0, 10));
-    const formattedDate = new Intl.DateTimeFormat("en-US", {
-        month: "long",
-        year: "numeric",
-    }).format(rawDate);
+
 
      return (
         <div className="w-96 bg-white border border-gray-300 p-6 m-1 rounded-2xl tracking-wide shadow-lg">
@@ -13,7 +10,7 @@ export default function CertificationCard({ blok }) {
                 <div className="leading-5 ml-6 sm">
                     <h4 className="text-lg font-semibold">{blok.name}</h4>
                     <h5 className="font-semibold">{blok.organization}</h5>
-                    <h6 className="text-sm font-semibold">{formattedDate}</h6>
+                    <h6 className="text-sm font-semibold">{formatDate(blok.date)}</h6>
                 </div>
             </div>
         </div>
