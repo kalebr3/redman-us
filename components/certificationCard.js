c509c05e3bede218ad9af19ac4ab9f921ad86236
Kaleb Redman <kaleb@redman.us>
2021-04-12 17:08:52 -0400

    fix broken education page on iOS

--- components/certificationCard.js
+++ components/certificationCard.js
@@ -8,7 +8,7 @@ export default function CertificationCard({ blok }) {
                 <div className="leading-5 ml-6 sm">
                     <h4 className="text-lg font-semibold">{blok.name}</h4>
                     <h5 className="font-semibold">{blok.organization}</h5>
-                    <h6 className="text-sm font-semibold">{Intl.DateTimeFormat("en-US", {month: "long", year: "numeric"}).format(date)}</h6>
+                    <h6 className="text-sm font-semibold">{new Intl.DateTimeFormat("en-US", {month: "long", year: "numeric"}).format(date)}</h6>
                 </div>
             </div>
         </div>
