745d9328dc68e4366a52655e77f1e7fe410dd44e
Kaleb Redman <kaleb@redman.us>
2021-04-12 17:14:18 -0400

    change formatted date to const

--- components/certificationCard.js
+++ components/certificationCard.js
@@ -1,5 +1,9 @@
 export default function CertificationCard({ blok }) {
-    const date = new Date(blok.date)
+    const rawDate = new Date(blok.date)
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+        month: "long",
+        year: "numeric",
+    }).format(rawDate)
 
      return (
         <div className="w-96 bg-white border border-gray-300 p-6 m-1 rounded-2xl tracking-wide shadow-lg">
@@ -8,11 +12,9 @@ export default function CertificationCard({ blok }) {
                 <div className="leading-5 ml-6 sm">
                     <h4 className="text-lg font-semibold">{blok.name}</h4>
                     <h5 className="font-semibold">{blok.organization}</h5>
-                    <h6 className="text-sm font-semibold">Date goes here!</h6>
+                    <h6 className="text-sm font-semibold">{formattedDate}</h6>
                 </div>
             </div>
         </div>
     )
 }
-
-// {new Intl.DateTimeFormat("en-US", {month: "long", year: "numeric"}).format(date)}
