2e7173001681ccd013d78653e408d48316172e0f
Kaleb Redman <kaleb@redman.us>
2021-04-12 21:38:43 -0400

    change project card images to full round

--- components/projectCard.js
+++ components/projectCard.js
@@ -16,7 +16,7 @@ export default function ProjectCard({ blok }) {
     return (
         <div className="w-96 bg-white border border-gray-300 p-6 m-1 rounded-2xl tracking-wide shadow-lg">
             <div className="flex items-center">
-                {blok.image.filename ? <img src={blok.image.filename} className="w-20" /> : null}
+                {blok.image.filename ? <img src={blok.image.filename} className="w-20 rounded-full" /> : null}
                 <div className="leading-5 ml-6 sm">
                     <h4 className="text-lg font-semibold">{blok.name}</h4>
                     <h6 className="text-sm font-semibold">
