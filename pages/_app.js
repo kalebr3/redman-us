f900684493e9fcf2f2234761a3e6aabb2c015a75
Kaleb Redman <kaleb@redman.us>
2021-04-13 21:43:06 -0400

    Implement Google Analytics GA4

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+
+import * as gtag from 'lib/gtag'
 
 // Import TailwindCSS
 import 'tailwindcss/tailwind.css'
@@ -9,6 +13,17 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fab)
 
 export default function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageView(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
         <>
             <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from 'lib/gtag'
+
 class MyDocument extends Document {
 
   render() {
     return (
       <Html lang="en">
         <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });`,
+          }} />
           {/* Adobe Fonts */}
           <link rel="stylesheet" href="https://use.typekit.net/wmo8lvj.css" />
         </Head>
