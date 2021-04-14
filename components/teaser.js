a4ea7aa97e43c91ee88564fbaa3cd137490bf12b
Kaleb Redman <kaleb@redman.us>
2021-04-14 17:44:37 -0400

    center teaser text

--- components/teaser.js
+++ components/teaser.js
@@ -2,7 +2,7 @@ import { createMarkup } from 'lib/storyblok'
 
 const Teaser = ({blok}) => {
     return (
-        <div className="grid place-items-center mt-10 xl:h-40 md:h-60 h-96 text-gray-500 text-xl px-6" dangerouslySetInnerHTML={createMarkup(blok.description)} />
+        <div className="grid place-items-center mt-10 xl:h-40 md:h-60 h-96 text-gray-500 text-xl text-center px-6" dangerouslySetInnerHTML={createMarkup(blok.description)} />
     )
 }
 
