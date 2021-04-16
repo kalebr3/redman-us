32fc114cacb80e69efa7ea705677462bf063cce7
Kaleb Redman <kaleb@redman.us>
2021-04-16 17:31:47 -0400

    remove un-needed div tag

--- components/modal.js
+++ components/modal.js
@@ -2,7 +2,7 @@ import CloseIcon from 'components/svg/closeIcon'
 
 export default function Modal({ onClick, header, body, footer }) {
     return (
-        <div>
+        <>
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed z-50 outline-none inset-0">
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -54,6 +54,6 @@ export default function Modal({ onClick, header, body, footer }) {
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
